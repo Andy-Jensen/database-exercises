7fbf859917ce587cd4747a1cf9dee0c24c079917
Andy Jensen <andrew.j.jensen91@gmail.com>
2022-11-01 17:02:40 -0500

    completed more exercises

--- more_exercises.sql
+++ more_exercises.sql
@@ -37,3 +37,56 @@ SELECT DISTINCT postal_code FROM address;
 /*
 Select all distinct (different) ratings from the film table.
 */
+SELECT DISTINCT rating FROM film_list;
+
+/*
+WHERE clause
+
+Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.
+*/
+SELECT * FROM payment;
+SELECT title, description, rating, length
+FROM film
+WHERE length >= 180;
+/*
+Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
+*/
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE payment_date >= '2005-05-27'
+ORDER BY payment_date;
+/*
+Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
+*/
+DESCRIBE payment;
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE payment_date LIKE '2005-05-27%';
+/*
+Select all columns from the customer table for rows that have a last names beginning with S and a first names ending with N.
+*/
+SELECT * FROM customer;
+SELECT *
+FROM customer
+WHERE first_name LIKE '%n' AND last_name LIKE 'S%';
+/*
+Select all columns from the customer table for rows where the customer is inactive or has a last name beginning with "M".
+*/
+SELECT *
+FROM customer
+WHERE active = '0' OR last_name LIKE 'M%';
+/*
+Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either C, S or T.
+*/
+DESCRIBE category;
+SELECT * FROM category;
+SELECT *
+FROM category
+WHERE category_id > 4 AND `name` LIKE 'C%' OR `name` LIKE 'S%' OR `name` LIKE 'T%';
+/*
+Select all columns minus the password column from the staff table for rows that contain a password.
+*/
+
+/*
+Select all columns minus the password column from the staff table for rows that do not contain a password.
+*/
